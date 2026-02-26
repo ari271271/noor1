5d21e261944b4f72c9e22e756a28893c7cea1161
ahmed adnan <ariadnanmanaf@gmail.com>
2026-02-26 22:00:09 +0300

    ekleme yaptım7

--- noor.js
+++ noor.js
@@ -352,17 +352,6 @@ function switchCasePage(pageNum) {
     window.scrollTo({ top: sectionTop - 100, behavior: 'smooth' });
 }
 
-// ÜRÜN FİLTRELEME FONKSİYONU
-function filterProducts(inputId, cardClass) {
-    const input = document.getElementById(inputId).value.toLowerCase();
-    const cards = document.querySelectorAll(cardClass);
-    
-    cards.forEach(card => {
-        const text = card.innerText.toLowerCase();
-        card.style.display = text.includes(input) ? "block" : "none";
-    });
-}
-
 // SCROLL OBSERVER (KAYDIRMA İZLEYİCİ)
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
